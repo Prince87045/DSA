8c5e53b07cf1472998f8cce226a2ed88458f814c
Prince <princemehta76@gmail.com>
2022-12-01 07:32:48 +0530

    initial commit

--- PriorityQueue/HeapOperations.java
+++ PriorityQueue/HeapOperations.java
@@ -1,9 +1,9 @@
 import java.util.*;
-public class InsertInHeap {
+public class HeapOperations {
     static class Heap{
         ArrayList<Integer> arr=new ArrayList<>();
 
-        public static void add(int data){
+        public void add(int data){
             //add at last index
             arr.add(data);
 
@@ -15,33 +15,56 @@ public class InsertInHeap {
                 int temp=arr.get(x);
                 arr.set(x,arr.get(par));
                 arr.set(par,temp);
+
+                x=par;
+                par=(x-1)/2;
             }
         }
 
-        public static int peek(){
+        public int peek(){//returns min value in heap
             return arr.get(0);
         }
 
-        /*private void heapify(int i){
+        private void heapify(int i){
             int left=2*i+1;
             int right=2*i+2;
             int minIdx=i;
 
-            if(left<arr.size()-1 && ){}
+            if(left<arr.size()-1 && arr.get(minIdx)>arr.get(left)){
+                minIdx=left;
+            }
+
+            if(right<arr.size()-1 && arr.get(minIdx)>arr.get(right)){
+                minIdx=right;
+            }
+
+            if(minIdx != i){
+                //swapping
+                int temp=arr.get(i);
+                arr.set(i,arr.get(minIdx));
+                arr.set(minIdx,temp);
+
+                heapify(minIdx);
+            }
         }
-        public static int remove(){
+        public int remove(){
+            int data=arr.get(0);
             //Step 1 - swap first and last
             int temp=arr.get(0);
             arr.set(0,arr.get(arr.size()-1));
-            arr.set(arr.size()-1,arr.get(0));
+            arr.set(arr.size()-1,temp);
 
             //Step 2 - delete last
             arr.remove(arr.size()-1);
 
             //step 3- heapify
-
+            heapify(0);
+            return data;
         }
-        */
+        public boolean isEmpty(){
+            return arr.size() == 0;
+        }
+        
     }
     public static void main(String[] args) {
         Heap h=new Heap();
@@ -52,6 +75,9 @@ public class InsertInHeap {
         h.add(10);
         h.add(1);
         
-        
+        while(!h.isEmpty()){
+            System.out.println(h.peek());
+            h.remove();
+        }
     }
 }
